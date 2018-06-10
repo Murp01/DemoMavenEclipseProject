f639324701e1c61a4f58fa8910585a832d0d0aac
Murp01 <pmurphy35@hotmail.com>
2018-06-10 14:16:25 +0100

    Added my own methods to call Linklaters.com and navigate its site
    Got fed up of looking at problems.

--- src/test/java/seleniumTests/E2E_Test.java
+++ src/test/java/seleniumTests/E2E_Test.java
@@ -1,8 +1,11 @@
 package seleniumTests;
 
+
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class E2E_Test {
@@ -11,9 +14,14 @@ public class E2E_Test {
 	public static void main(String[] args) {
 		driver = new FirefoxDriver();		
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);		
-		driver.get("http://www.store.demoqa.com");
+		driver.get("https://www.linklaters.com/");
 		
-		driver.navigate().to("http://shop.demoqa.com/?s=" + "dress" + "&post_type=product");
+		//Click on Insights title
+		driver.findElement(By.xpath("/html/body/header/div/div/div/ul/li[6]/a")).click();
+		
+		//Enter paul into the search box
+		driver.findElement(By.xpath("/html/body/div[4]/div/div/div[1]/div/div[1]/div[1]/input")).sendKeys("Paul");
+		
 
 	}
 
