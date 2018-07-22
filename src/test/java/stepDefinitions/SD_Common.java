2b5fba8f363f4b7d5ff55bdae453dbdbf1ef1fed
Murp01 <pmurphy35@hotmail.com>
2018-07-22 17:02:21 +0100

    Cleaned up SD_Common (alphabetized)

--- src/test/java/stepDefinitions/SD_Common.java
+++ src/test/java/stepDefinitions/SD_Common.java
@@ -17,61 +17,67 @@ public class SD_Common extends AbstractPageStepDefinition{
 	WebDriver driver = getDriver();
 	Common common; 
 	
-	@Given("^clicks on Insights on the title header$")
-	public void clicks_on_Insights_on_the_title_header() throws Throwable {
-		common = new Common(driver);
-		common.insightTitleClick();
-	}
+	@Then("^Assert that the url contains \"([^\"]*)\"$")
+	public void assert_that_the_url_contains(String title) throws Throwable {
+		Assert.assertTrue(driver.getTitle().contains(title));
+	}	
 	
-	@Given("^clicks on the search toggle$")
-	public void clicks_on_the_search_icon() throws Throwable {
-		common = new Common(driver);
-		common.searchToggleClick();
-	}
 	
-	@Given("^hovers over the Insights title$")
-	public void hovers_over_the_Insights_title() throws Throwable {
-		common = new Common(driver);
-	    common.insightTitleHover();
-	}
-	
-	@Given("^the search input will open$")
-	public void the_search_box_will_open() throws Throwable {
-		common = new Common(driver);
-		common.searchInputAssertTrue();
-	}
-	
-	@Given("^the search input will not be open$")
-	public void the_search_box_will_not_be_open() throws Throwable {
-		common = new Common(driver);
-		common.searchInputAssertFalse();
-	}
-	
-	@Given("^the cookie policy banner appears$")
-	public void the_cookie_policy_banner_appears() throws Throwable {
-		common = new Common(driver);
-		common.cookieBannerAssertTrue();
-	}
-	
-	@Given("^clicks on the \"([^\"]*)\" link on the cookie banner$")
-	public void clicks_on_the_link_on_the_cookie_banner(String arg1) throws Throwable {
+	@Given("^clicks on the Blogs link from the secondary navigation$")
+	public void clicks_on_the_Blogs_link_from_the_secondary_navigation() throws Throwable {
 
 	}
-
+	
 	@Given("^clicks the close button on the cookie banner$")
 	public void clicks_the_close_button_on_the_cookie_banner() throws Throwable {
 		common = new Common(driver);
 		common.cookieBannerCloseBtn();
 	}
 	
-	@Then("^the cookie policy banner will not be displayed$")
-	public void the_cookie_policy_banner_will_not_be_displayed() throws Throwable {
-
+	@Given("^clicks on Insights on the title header$")
+	public void clicks_on_Insights_on_the_title_header() throws Throwable {
+		common = new Common(driver);
+		common.insightTitleClick();
+	}
+	
+	@Given("^hovers over the About Us title and selects \"([^\"]*)\"$")
+	public void hovers_over_the_About_Us_title_and_selects(String link) throws Throwable {
+		switch(link) {
+		case "Alumni":
+			Actions action = new Actions(driver);
+			action.moveToElement(driver.findElement(By.xpath("//div[@class='header__navDesktop']/ul/li/a[@href='/en/about-us']"))).click().build().perform();
+			driver.findElement(By.xpath("//li/a[@href='/en/about-us/alumni']")).click();  //not finding correct link
+			break;
+		case "At a glance":
+			driver.findElement(By.xpath("//a[@href='/en/client-services'][1]")).click();
+			break;
+		case "Contact Us":
+			driver.findElement(By.xpath("//a[@href='/en/sectors'][1]")).click();
+			break;
+		case "Executive Committee":
+			driver.findElement(By.xpath("//a[@href='/en/find-a-lawyer'][1]")).click();
+			break;
+		case "Innovation":
+			driver.findElement(By.xpath("//a[@href='/en/locations'][1]")).click();
+			break;
+		case "News and Deals":
+			driver.findElement(By.xpath("//a[@href='/en/insights'][1]")).click();
+			break;
+		case "Responsibility":
+			driver.findElement(By.xpath("//a[@href='/en/careers'][1]")).click();
+			break;	
+		}
 	}
 	
 	@Given("^clicks on the Linklaters logo$")
 	public void clicks_on_the_Linklaters_logo() throws Throwable {
 
+	}
+	
+	@Given("^clicks on the search toggle$")
+	public void clicks_on_the_search_icon() throws Throwable {
+		common = new Common(driver);
+		common.searchToggleClick();
 	}
 	
 	@When("^clicks on the \"([^\"]*)\" link in the primary navigation$")
@@ -100,45 +106,33 @@ public class SD_Common extends AbstractPageStepDefinition{
 			break;	
 		}
 	}
-		
-	@Given("^hovers over the About Us title and selects \"([^\"]*)\"$")
-	public void hovers_over_the_About_Us_title_and_selects(String link) throws Throwable {
-		switch(link) {
-		case "Alumni":
-			Actions action = new Actions(driver);
-			action.moveToElement(driver.findElement(By.xpath("//div[@class='header__navDesktop']/ul/li/a[@href='/en/about-us']"))).click().build().perform();
-			driver.findElement(By.xpath("//li/a[@href='/en/about-us/alumni']")).click();  //not finding correct link
-			break;
-		case "At a glance":
-			driver.findElement(By.xpath("//a[@href='/en/client-services'][1]")).click();
-			break;
-		case "Contact Us":
-			driver.findElement(By.xpath("//a[@href='/en/sectors'][1]")).click();
-			break;
-		case "Executive Committee":
-			driver.findElement(By.xpath("//a[@href='/en/find-a-lawyer'][1]")).click();
-			break;
-		case "Innovation":
-			driver.findElement(By.xpath("//a[@href='/en/locations'][1]")).click();
-			break;
-		case "News and Deals":
-			driver.findElement(By.xpath("//a[@href='/en/insights'][1]")).click();
-			break;
-		case "Responsibility":
-			driver.findElement(By.xpath("//a[@href='/en/careers'][1]")).click();
-			break;	
-		}
-	}	
 	
-	@Given("^clicks on the Blogs link from the secondary navigation$")
-	public void clicks_on_the_Blogs_link_from_the_secondary_navigation() throws Throwable {
+	@Given("^clicks on the \"([^\"]*)\" button on the social media banner$")
+	public void clicks_on_the_button_on_the_social_media_banner(String arg1) throws Throwable {
 
 	}
 	
-	@Then("^Assert that the url contains \"([^\"]*)\"$")
-	public void assert_that_the_url_contains(String title) throws Throwable {
-		Assert.assertTrue(driver.getTitle().contains(title));
-	}	
+	@Given("^clicks on the \"([^\"]*)\" link on the cookie banner$")
+	public void clicks_on_the_link_on_the_cookie_banner(String arg1) throws Throwable {
+
+	}
+	
+	@Given("^clicks on the \"([^\"]*)\" link from the footer$")
+	public void clicks_on_the_link_from_the_footer(String arg1) throws Throwable {
+
+	}
+	
+	@Given("^the cookie policy banner appears$")
+	public void the_cookie_policy_banner_appears() throws Throwable {
+		common = new Common(driver);
+		common.cookieBannerAssertTrue();
+	}
+	
+	@Given("^hovers over the Insights title$")
+	public void hovers_over_the_Insights_title() throws Throwable {
+		common = new Common(driver);
+	    common.insightTitleHover();
+	}
 	
 	@Given("^hovers over the insights title and selects publications$")
 	public void hovers_over_the_insights_title_and_selects_publications() throws Throwable {
@@ -147,14 +141,22 @@ public class SD_Common extends AbstractPageStepDefinition{
 		driver.findElement(By.xpath("/html/body/nav/div/div[6]/ul[3]/li/a")).click();
 	}
 	
-	@Given("^clicks on the \"([^\"]*)\" button on the social media banner$")
-	public void clicks_on_the_button_on_the_social_media_banner(String arg1) throws Throwable {
+	@Then("^the cookie policy banner will not be displayed$")
+	public void the_cookie_policy_banner_will_not_be_displayed() throws Throwable {
 
 	}
-
-	@Given("^clicks on the \"([^\"]*)\" link from the footer$")
-	public void clicks_on_the_link_from_the_footer(String arg1) throws Throwable {
-
+	
+	@Given("^the search input will not be open$")
+	public void the_search_box_will_not_be_open() throws Throwable {
+		common = new Common(driver);
+		common.searchInputAssertFalse();
 	}
+	
+	@Given("^the search input will open$")
+	public void the_search_box_will_open() throws Throwable {
+		common = new Common(driver);
+		common.searchInputAssertTrue();
+	}
+
 
 }
