facf9713cfd71ad0d0a52ec5027930a556f357b1
pmurp <pmurp@YogaFire-LP>
2018-10-01 22:03:26 +0100

    Moved test runner into resources

--- src/test/resources/runners/TestRunner.java
+++ src/test/resources/runners/TestRunner.java
@@ -8,6 +8,7 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(
 		features = "src/test/resources/functionalTests"
 		,glue = {"stepDefinitions","stepDefinitionsCareers"}
+		,plugin = {"json:target/cucumber-parallel/1.json", "html:target/cucumber-parallel/1.html", "pretty"}
 		,monochrome = true
 		,tags = {"@Trythis"}
 		)
